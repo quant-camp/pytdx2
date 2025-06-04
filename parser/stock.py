ca51124d3cbe1fb6f756114ffa0ab2bbb3c4798a
LisonEvf <LisonEvf@gmail.com>
2025-06-04 22:50:41 +0800

    feat(tdxClient): 添加get_security_quotes_by_category方法
    新增通过板块分类获取股票行情的方法，支持分页参数
    ```
    
    ```msg
    refactor(parser/stock): 重构行情解析逻辑
    
    1. 重命名ListOld为ListB以保持命名一致性
    2. 优化QuotesDetail和QuotesList的解析结构，使用循环处理买卖盘数据
    3. 添加Unusual类处理主力监控数据，完善异常交易类型描述
    4. 统一未知字段的命名规范为unknown前缀
    ```
    
    ```msg
    test(tdxClient): 更新主函数测试用例
    
    调整测试用例顺序，增加对服务器公告、升级提示等接口的测试
    移除被注释的旧测试代码，保持测试用例简洁

--- parser/stock.py
+++ parser/stock.py
@@ -102,23 +102,24 @@ class List(BaseParser):
         stocks = []
         for i in range(count):
             pos = 2 + i * 37
-            (code, vol, name, _, unknown1, decimal_point, pre_close, unknown2) = struct.unpack("<6sH8s8s4sBf4s", data[pos: pos + 37])
+            (code, vol, name, _, unknown1, decimal_point, pre_close, unknown2, unknown3) = struct.unpack("<6sH8s8s4sBfHH", data[pos: pos + 37])
             code = code.decode('gbk', errors='ignore').rstrip('\x00')
             name = name.decode('gbk', errors='ignore').rstrip('\x00')
 
+            # print(data[pos: pos + 37].hex())
             stocks.append({
                 'code': code,
                 'vol': vol,
                 'name': name,
                 'decimal_point': decimal_point,
                 'pre_close': pre_close,
-                'unknown1': [unknown1.hex(), unknown2.hex()],
+                'unknown1': [unknown1.hex(), unknown2, unknown3],
             })
 
         return stocks
 
 @register_parser(0x450)
-class ListOld(BaseParser):
+class ListB(BaseParser):
     def __init__(self, market: MARKET, start):
         self.body = struct.pack(u'<HH', market.value, start)
 
@@ -129,18 +130,17 @@ class ListOld(BaseParser):
         stocks = []
         for i in range(count):
             pos = 2 + i * 29
-            (code, vol, name, unknown1, decimal_point, pre_close, unknown2) = struct.unpack("<6sH8s4sBf4s", data[pos: pos + 29])
+            (code, vol, name, unknown1, decimal_point, pre_close, unknown2, unknown3) = struct.unpack("<6sH8s4sBfHH", data[pos: pos + 29])
             code = code.decode('gbk', errors='ignore').rstrip('\x00')
             name = name.decode('gbk', errors='ignore').rstrip('\x00')
-
+            
             stocks.append({
                 'code': code,
                 'vol': vol,
                 'name': name,
                 'decimal_point': decimal_point,
                 'pre_close': pre_close,
-                'unknown1': unknown1.hex(),
-                'unknown2': unknown2.hex(),
+                'unknown1': [unknown1.hex(), unknown2, unknown3],
             })
 
         return stocks
@@ -306,7 +306,31 @@ class ChartSampling(BaseParser):
             'price': price,
             'prices': prices
         }
-        
+
+def _format_time(time_stamp):
+    if time_stamp == 0:
+        return '00:00:00.000'
+    else:
+        time_stamp = str(time_stamp)
+    """
+    format time from reversed_bytes0
+    by using method from https://github.com/rainx/pytdx/issues/187
+    """
+    time = time_stamp[:-6] + ':'
+    if int(time_stamp[-6:-4]) < 60:
+        time += '%s:' % time_stamp[-6:-4]
+        time += '%06.3f' % (
+            int(time_stamp[-4:]) * 60 / 10000.0
+        )
+    else:
+        time += '%02d:' % (
+            int(time_stamp[-6:]) * 60 / 1000000
+        )
+        time += '%06.3f' % (
+            (int(time_stamp[-6:]) * 60 % 1000000) * 60 / 1000000.0
+        )
+    return time
+
 @register_parser(0x53e)
 class QuotesDetail(BaseParser):
     def __init__(self, stocks: list[MARKET, str]):
@@ -338,7 +362,7 @@ class QuotesDetail(BaseParser):
             high, pos = get_price(data, pos)
             low, pos = get_price(data, pos)
             server_time, pos = get_price(data, pos)
-            reversed_bytes1, pos = get_price(data, pos)
+            after_hour, pos = get_price(data, pos)
             vol, pos = get_price(data, pos)
             cur_vol, pos = get_price(data, pos)
             
@@ -356,63 +380,41 @@ class QuotesDetail(BaseParser):
             s_amount, pos = get_price(data, pos) #reversed_bytes2
             b_amount, pos = get_price(data, pos) #reversed_bytes3
 
-            bid1, pos = get_price(data, pos)
-            ask1, pos = get_price(data, pos)
-            bid_vol1, pos = get_price(data, pos)
-            ask_vol1, pos = get_price(data, pos)
+            handi_cap = {
+                'bid': [],
+                'ask': [],
+            }
+            for _ in range(5): # 5个
+                bid, pos = get_price(data, pos)
+                ask, pos = get_price(data, pos)
+                bid_vol, pos = get_price(data, pos)
+                ask_vol, pos = get_price(data, pos)
 
-            bid2, pos = get_price(data, pos)
-            ask2, pos = get_price(data, pos)
-            bid_vol2, pos = get_price(data, pos)
-            ask_vol2, pos = get_price(data, pos)
+                bid += price
+                ask += price
 
-            bid3, pos = get_price(data, pos)
-            ask3, pos = get_price(data, pos)
-            bid_vol3, pos = get_price(data, pos)
-            ask_vol3, pos = get_price(data, pos)
+                handi_cap['bid'].append({
+                    'price': bid,
+                    'vol': bid_vol,
+                })
+                handi_cap['ask'].append({
+                    'price': ask,
+                    'vol': ask_vol,
+                })
 
-            bid4, pos = get_price(data, pos)
-            ask4, pos = get_price(data, pos)
-            bid_vol4, pos = get_price(data, pos)
-            ask_vol4, pos = get_price(data, pos)
-
-            bid5, pos = get_price(data, pos)
-            ask5, pos = get_price(data, pos)
-            bid_vol5, pos = get_price(data, pos)
-            ask_vol5, pos = get_price(data, pos)
-
-            bid1 += price
-            ask1 += price
-            bid2 += price
-            ask2 += price
-            bid3 += price
-            ask3 += price
-            bid4 += price
-            ask4 += price
-            bid5 += price
-            ask5 += price
-
-            (reversed_bytes4,) = struct.unpack('<H', data[pos: pos + 2])
-            pos += 2
-
-            reversed_bytes5, pos = get_price(data, pos)
-            reversed_bytes6, pos = get_price(data, pos)
-            reversed_bytes7, pos = get_price(data, pos)
-            reversed_bytes8, pos = get_price(data, pos)
-
-            (reversed_bytes9, active2) = struct.unpack("<hH", data[pos: pos + 4])
-            pos += 4
+            (v1, unknown, v2, active2) = struct.unpack('<H4shH', data[pos: pos + 10])
+            pos += 10
 
             quotes.append({
                 'market': MARKET(market),
                 'code': code,
                 'price': price,
-                'last_close': last_close,
                 'open': open,
                 'high': high,
                 'low': low,
+                'last_close': last_close,
                 'server_time': server_time,
-                'reversed_bytes1': reversed_bytes1,
+                'after_hour': after_hour,
                 'vol': vol,
                 'cur_vol': cur_vol,
                 'amount': amount,
@@ -420,23 +422,12 @@ class QuotesDetail(BaseParser):
                 'b_vol': b_vol,
                 's_amount': s_amount,
                 'b_amount': b_amount,
-                'handi_cap': {
-                    'bid': [
-                        {'price': bid1, 'vol': bid_vol1},
-                        {'price': bid2, 'vol': bid_vol2},
-                        {'price': bid3, 'vol': bid_vol3},
-                        {'price': bid4, 'vol': bid_vol4},
-                        {'price': bid5, 'vol': bid_vol5},
-                    ],
-                    'ask': [
-                        {'price': ask1, 'vol': ask_vol1},
-                        {'price': ask2, 'vol': ask_vol2},
-                        {'price': ask3, 'vol': ask_vol3},
-                        {'price': ask4, 'vol': ask_vol4},
-                        {'price': ask5, 'vol': ask_vol5},
-                    ]
-                },
-                'unknown': [reversed_bytes4, reversed_bytes5, reversed_bytes6, reversed_bytes7, reversed_bytes8, reversed_bytes9],
+                # 'handi_cap': handi_cap,
+                'v1': v1,
+                'unknown': unknown.hex(),
+                'v2': v2,
+                'active1': active1,
+                'active2': active2,
             })
 
         return quotes
@@ -453,7 +444,7 @@ class QuotesDetail(BaseParser):
 
 # <b1cb7400 0c 7a002900 00 4705 6e00 6e00 |929392a5a3a3a3a3aac39837a1829a899360da91939316639625d884aa86de060090234f91d92a119d9391149294d290259281d197bb8ed0998996d79f1792a9459482a79393f5cca8db77a177a1939377a177a1939377a177a1939377a177a1939377a177a1939377a177a19393
 @register_parser(0x547)
-class Quotes2(BaseParser):
+class TODO547(BaseParser):
     def __init__(self, stocks: list[MARKET, str]):
         count = len(stocks)
         if count <= 0:
@@ -516,30 +507,45 @@ class QuotesList(BaseParser):
             s_amount, pos = get_price(data, pos) #reversed_bytes2
             b_amount, pos = get_price(data, pos) #reversed_bytes3
 
-            bid1, pos = get_price(data, pos)
-            ask1, pos = get_price(data, pos)
-            bid_vol1, pos = get_price(data, pos)
-            ask_vol1, pos = get_price(data, pos)
+            handi_cap = {
+                'bid': [],
+                'ask': [],
+            }
+            for _ in range(1): # 5个
+                bid, pos = get_price(data, pos)
+                ask, pos = get_price(data, pos)
+                bid_vol, pos = get_price(data, pos)
+                ask_vol, pos = get_price(data, pos)
 
-            bid1 += price
-            ask1 += price
+                bid += price
+                ask += price
 
-            print(data[pos: pos + 54].hex())
-            (v1, v2, v3 ,v4, v5, v6, v7, v8, v9, _) = struct.unpack('<HHBBBHBBB10s', data[pos: pos + 22])
+                handi_cap['bid'].append({
+                    'price': bid,
+                    'vol': bid_vol,
+                })
+                handi_cap['ask'].append({
+                    'price': ask,
+                    'vol': ask_vol,
+                })
+
+            # (v1, v2, unknown1, _) = struct.unpack('<HhBBfBB10s', data[pos: pos + 22])
+            (v1, v2, unknown1, _) = struct.unpack('<Hh8s10s', data[pos: pos + 22])
             pos += 22
-            (a, b, _) = struct.unpack('<II24s', data[pos: pos + 32])
+            # (unknown2, unknown22, _) = struct.unpack('<ff24s', data[pos: pos + 32])
+            (unknown2, _) = struct.unpack('<8s24s', data[pos: pos + 32])
             pos += 32
-            active2 = struct.unpack('<H', data[pos: pos + 2]) # == active1
+            active2, = struct.unpack('<H', data[pos: pos + 2]) # == active1
             pos += 2
 
             stocks.append({
                 'market': MARKET(market),
                 'code': code.decode('gbk'),
                 'price': price,
-                'last_close': last_close,
                 'open': open,
                 'high': high,
                 'low': low,
+                'last_close': last_close,
                 'server_time': server_time,
                 'after_hour': after_hour,
                 'vol': vol,
@@ -549,38 +555,16 @@ class QuotesList(BaseParser):
                 'b_vol': b_vol,
                 's_amount': s_amount,
                 'b_amount': b_amount,
-                'handi_cap': {
-                    'bid': {'price': bid1, 'vol': bid_vol1},
-                    'ask': {'price': ask1, 'vol': ask_vol1}
-                },
-                'unknown': [v1, v2, v3, v4, v5],
-                'unknown2': [v6, v7, v8, v9, a, b],
+                # 'handi_cap': handi_cap,
+                'v1': v1,
+                'v2': v2,
+                'unknown1': unknown1.hex(),
+                'unknown2': unknown2.hex(),
+                'active1': active1,
+                'active2': active2,
             })
         return stocks
 
-def _format_time(time_stamp):
-    if time_stamp == 0:
-        return '00:00:00.000'
-    else:
-        time_stamp = str(time_stamp)
-    """
-    format time from reversed_bytes0
-    by using method from https://github.com/rainx/pytdx/issues/187
-    """
-    time = time_stamp[:-6] + ':'
-    if int(time_stamp[-6:-4]) < 60:
-        time += '%s:' % time_stamp[-6:-4]
-        time += '%06.3f' % (
-            int(time_stamp[-4:]) * 60 / 10000.0
-        )
-    else:
-        time += '%02d:' % (
-            int(time_stamp[-6:]) * 60 / 1000000
-        )
-        time += '%06.3f' % (
-            (int(time_stamp[-6:]) * 60 % 1000000) * 60 / 1000000.0
-        )
-    return time
 # >0c f32d8800 01 8300 8300 4c05 | 0500 00000000 0000 1100 0030303030303100333030373636003330303739300033303132333600333030363537003330303932360033303036303401363033383933003030303135350033303034333201363035353938003030323437320136303035343700303032323631003330303036350030303236343000333030393633
 # <b1cb7400 1c ff2d8800 00 4c05 a304 df06 789c75946b4c53671cc6ffef29d07268cfa1078a06499529c36583f50eac5b4e41702ba5355ec6d06d06dd07972c448dbbbb0fb55ccbc5225044a86ca52d051c2a9d651d552a8439c7580ec6d9396288ec6296a84159b23042ec4edd87950fe73ddf9f5f9ee7f7be07400c208b1eb95b5c4395eab1326b9821a628676d23b2df89771fe831d55af21dd5bbc05e8d01aadf5f571ce62f4770a8bc602a839833f551a52edbface24701cb7189432599e465321f6ab43c88efad11b5d3e8670e87b1bb774bcf2d9dc31d3e074aa3f44c2375e1cd057d8304a03216482b9c464880d32fdf31bfd60f67c2117a8e23f50818c491a975a913a7fbbed7b8600f745a10337ddd1192fdfe60ffc1c078e4554085bfd2c64352200e875ea0391ff43bc171b68d144791117844962217285527330d9bdd38ddc6810550e7632c4657ce412657e4116d49abc37139b160410b0251442b24510a632810028ab34fc1dc3193dfe8c4e6b19e05ced6072b48c469d372f1acb6e47cdecb7abfd2a5be7eb3e4953bae3e14fc68bcbc286210138fcac9d1e891b8545a99004e6164b696ccef8cc197a75e3ab212ecebc28ca295068ecfca12cad5259ea9a6421b6ae3867fcbec5b365be5bd8d949042324a056bc0985e301f860f6edd0c7c56404b45d747be56ececdecfca75d64aa2ea15da7557d894c012f430c90b67ebc59a4edeb37facf2574aff0e0ca186b6610eb45e144009624d9531a8999acff5e1b7de434ce89e912228d4c995fa01491bd0ba88fe7c25cd83554eff510f30bc86f891f4cdff0a9c8e432f3da2c98d4729da7073e9e96940624803f68588d211dd95ba25bda7b7a828b2422a34f47ae5607052364711159ecb8cd108fc9b6135477fa8bfb09a3cf8c3bcd0268395e08b6cccec4cef5a9c0eeb575561f1bb270789a7e98d5cc799d8382e86a2aa52244546f50c9d5fac61f19a223c7f75dc6a92d39af4f1a5d36716b200942bfb0fef1b872c9efd1db9cffc11afb2d7f35d29b6f49396f5988602753ab0bf219deb94d14f55289e57e3f717f93e501aa4389b62543cdc7a16370663d207e575c5a2aceb6303f225e8b4d7836a3963ede769893c0f0d8a914aa3c45067941de82aa510d32f48e32c4a2bcd5233a4f9ebfeb315eea13382cf1f0c5095404025e38310178603eb2adf4498c11cdf0fbf46d9d8cd37d06c91691a955793d6420b74c2cdd7366be9f58cef505c481b79a6fcc18af581247fd82a2ee511ea0512cb7935a89c4c15dbc61cd0fc6932dd57dd2b8c4a9bd27aa5da1d0c80f5123396d48a5daed1863bde7b8e6b60fbddb609936d96f649fb46e045f3d5108766187b413e19002ba61fb1a8ceac359facfc81427e6101515cfbefda3d450c67d348169e630d70f0c11f0f0badba75053007bfbd8f33b4eac56749f7e0face30240d6dc9697d3a8bb9172d05d4b5af373ce5ab9498f38b339f51ca5a295342a5915e5e14b92538a9b7f65883ff8b5de792c38c77b5ce535390702c8f3c483c07972732154b720fbb758f93a0914003caa34ccc4643d97de43d70977723aaa7adaab40a3046a206798ef8e3f99b0c7c30ed89a1f9adfe67622d9beeba6dacfb38221299c5bdd0aa82da50e0baf5b8984e1ea9bf70c793149117a932e7cc0c94902ea5f8efca387
 @register_parser(0x54c)
@@ -595,7 +579,7 @@ class Quotes(QuotesList):
 
 
 @register_parser(0x563)
-class Lists(BaseParser):
+class Unusual(BaseParser): # 主力监控
     def __init__(self, market: MARKET, start: int, count: int = 600):
         self.body = struct.pack('<HII', market.value, start, count)
     @override
@@ -606,17 +590,104 @@ class Lists(BaseParser):
         for i in range(count):
             pice_data = data[32 * i + 2: 32 * (i + 1) + 2]
 
-            market, code, u1, u2, index, z = struct.unpack('<H6sBHHH', pice_data[:15])
-            v1, v2, v3, v4, v5, v6, v7, v8 = struct.unpack('<BBHIIHBH', pice_data[15: 32])
+            market, code, _, type, _, index, z = struct.unpack('<H6sBBBHH', pice_data[:15])
+            hour, minute_sec = struct.unpack('<BH', pice_data[29: 32])
 
-            print(pice_data.hex())
+            type, val = self.unpack_by_type(type, pice_data[15: 28])
+
+            # print(pice_data.hex())
             stocks.append({
+                "index": index,
                 "market": MARKET(market),
                 "code": code.decode('gbk').replace('\x00', ''),
-                "u": [u1, u2],
-                "index": index,
-                "a": [v1, v2, v3],
-                "b": [v4, v5, v6],
-                "c": [v7, v8]
+                "time": f"{hour:02d}:{minute_sec // 100:02d}:{minute_sec % 100:02d}",
+                "type": type,
+                "val": val,
             })
         return stocks
+    
+    def unpack_by_type(self, type: int, data: bytearray):
+        v1, v2, v3, v4 = struct.unpack('<Bfff', data)
+        desc = ''
+        val = ''
+        match type:
+            case 0x03: # 主力买入、卖出
+                if v1 == 0x00:
+                    desc = "主力买入"
+                else:
+                    desc = "主力卖出"
+                val = f"{v2:.2f}/{v3:.2f}"
+            case 0x04: # 加速拉升
+                desc = "加速拉升"
+                val = f"{v2*100:.2f}%"
+            case 0x05: # 加速下跌
+                desc = "加速下跌"
+            case 0x06: # 低位反弹
+                desc = "低位反弹"
+                val = f"{v2*100:.2f}%"
+            case 0x07: # 高位回落
+                desc = "高位回落"
+                val = f"{v2*100:.2f}%"
+            case 0x08: # 撑杆跳高
+                desc = "撑杆跳高"
+                val = f"{v2*100:.2f}%"
+            case 0x09: # 平台跳水
+                desc = "平台跳水"
+                val = f"{v2*100:.2f}%"
+            case 0x0a: # 单笔冲涨、跌
+                desc = "单笔冲" + ("跌" if v2 < 0x00 else "涨")
+                val = f"{v2*100:.2f}%"
+            case 0x0b: # 区间放量 涨、跌、平
+                desc = "区间放量"
+                val = f"{v2:.1f}倍"
+                if v3 == 0:
+                    desc += "平"
+                else:
+                    desc += "跌" if v3 < 0 else "涨"
+                    val += f"{v3*100:.2f}%"
+            case 0x0c: # 区间缩量
+                desc = "区间缩量"
+            case 0x10: # 大单托盘
+                desc = "大单托盘"
+                val = f"{v4:.2f}/{v3:.2f}"
+            case 0x11: # 大单压盘
+                desc = "大单压盘"
+                val = f"{v2:.2f}/{v3:.2f}"
+            case 0x12: # 大单锁盘
+                desc = "大单锁盘"
+            case 0x13: # 竞价试买
+                desc = "竞价试买"
+                val = f"{v2:.2f}/{v3:.2f}"
+            case 0x14: # 打开涨停
+                type, v2, v3 = struct.unpack('<Bff', data[1:10])
+                direction = "涨" if v1 == 0x00 else "跌"
+                if type == 0x01:
+                    desc = f"逼近{direction}停"
+                elif type == 0x02:
+                    desc = f"封{direction}停板"
+                elif type == 0x04:
+                    desc = f"封{direction}大减"
+                elif type == 0x05:
+                    desc = f"打开{direction}停"
+                val = f"{v2:.2f}/{v3:.2f}"
+            case 0x15: # 尾盘
+                if v1 == 0x00:
+                    desc = "尾盘??"
+                elif v1 == 0x01:
+                    desc = "尾盘对倒"
+                elif v1 == 0x02:
+                    desc = "尾盘拉升"
+                else:
+                    desc = "尾盘???"
+                val = f"{v2*100:.2f}%/{v3:.2f}"
+            case 0x16: # 盘中弱势、强势
+                desc = "盘中" + ("弱势" if v2 < 0x00 else "强势")
+                val = f"{v2*100:.2f}%"
+            case 0x1d: # 急速拉升
+                desc = "急速拉升"
+                val = f"{v2*100:.2f}%"
+            case 0x1e: # 急速下跌
+                desc = "急速下跌" 
+                val = f"{v2*100:.2f}%"
+            
+        return desc, val
